935a94c6e6e0b730c74bb74e984fb87c5933432a
kim dong hyeon <kdh990315@naver.com>
2025-02-11 12:28:47 +0900

    feat api 엔드포인트 정의

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
     
     // OAuth 2.0 파라미터 설정
     const params = {
-      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID, // 구글 콘솔에 등록된 클라이언트 ID
       redirect_uri: import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URI, // 구글 콘솔에 등록된 리다이렉트 URI
-      response_type: 'token',
-      scope: 'email profile',
-      access_type: 'online',
+      response_type: 'token', // 토큰 반환 타입 
+      scope: 'email profile', // 요청 권한
+      access_type: 'online', // 액세스 타입
     };
 
     // URL 생성 및 리다이렉트
@@ -17,12 +17,9 @@ function App() {
     window.location.href = url;
   }
 
-  
-
   return (
     <div className="w-dvh h-dvh flex items-center justify-center">
       <Button onClick={googleLogin}>클릭!</Button>
-
     </div>
   );
 }
