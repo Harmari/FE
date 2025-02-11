c8a56b7833840de4488ca5e5ea9dc26ba0a17f9e
kim dong hyeon <kdh990315@naver.com>
2025-02-11 12:37:31 +0900

    feat axios 인스턴스 추가

---
+++ src/config/axiosDevConfig.js
@@ -0,0 +1,13 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+import axios from "axios";
+
+const devApi = axios.create({
+  baseURL: BASE_URL,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default devApi;
