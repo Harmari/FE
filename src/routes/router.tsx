262d67d7014c7af9dac65d25d475359120625e9e
Sean <joajoa70584@gmail.com>
2025-02-11 21:11:51 +0900

    fix: 라우터 Oauth 오타 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,16 +1,16 @@
 import KakaoTest from "@/KakaoTest";
-import Oauth from "@/oauth";
+import Oauth from "@/Oauth";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <KakaoTest />
-	},
-	{
-		path: "/oauth",
-		element: <Oauth />,
-	},
+  {
+    path: "/",
+    element: <KakaoTest />,
+  },
+  {
+    path: "/oauth",
+    element: <Oauth />,
+  },
 ]);
 
 export default router;
