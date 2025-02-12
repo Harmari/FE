2dd89d88fe8c580f73d354df653fcde10609062f
kim dong hyeon <kdh990315@naver.com>
2025-02-12 22:24:29 +0900

    fix: 예약리스트 api 수정

--- src/apis/reservation.ts
+++ src/apis/reservation.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { RESERVATION_ENDPOINT } from "./endpoints";
 
-export const getReservationList = async () => {
-  const user_id = "67ab499ba706f516fb348ddd";
+export const getReservationList = async (user_id: string) => {
   try {
     const response = await axios.get(RESERVATION_ENDPOINT.get_list(user_id));
     return response.data;
