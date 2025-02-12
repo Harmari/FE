de820fd17393134bcd97e956cfd40f3d572ee703
Sean <joajoa70584@gmail.com>
2025-02-12 20:36:48 +0900

    feat: 결제, 예약 페이지 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as LoginPage } from "./LoginPage";
 export { default as DesignerListPage } from "./DesignerListPage";
+export { default as PaymentPage } from "./PaymentPage";
+export { default as ReservationListPage } from "./ReservationListPage";
