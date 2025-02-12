af5e39f79302bf46743e2dd9d142a3b6559c2bcb
Sean <joajoa70584@gmail.com>
2025-02-12 20:36:48 +0900

    feat: 결제, 예약 페이지 추가

---
+++ src/pages/PaymentPage/index.tsx
@@ -0,0 +1,5 @@
+const PaymentPage = () => {
+  return <div>결제 페이지</div>;
+};
+
+export default PaymentPage;
---
+++ src/pages/ReservationListPage/index.tsx
@@ -0,0 +1,5 @@
+const ReservationListPage = () => {
+  return <div>예약 목록 페이지</div>;
+};
+
+export default ReservationListPage;
