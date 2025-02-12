8b15be06f31b993e3ac621982cd8d2d4d90ea23c
kim dong hyeon <kdh990315@naver.com>
2025-02-12 21:42:03 +0900

    feat: 디자이너 예약 리스트 api endpoint 추가

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,11 +1,12 @@
 export const serverURL = import.meta.env.VITE_API_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // 로컬 서버 엔드포인트
 export const localURL = "http://localhost:3001";
 
 interface ReservationEndpoint {
   list: string;
-  get_list: string;
+  get_list: (user_Id: string) => string;
 }
 
 interface LocalEndpoint {
@@ -20,7 +21,7 @@ interface LocalEndpoint {
 
 export const RESERVATION_ENDPOINT: ReservationEndpoint = Object.freeze({
   list: `${serverURL}/reservations/list`, // 디자이너 예약 목록 조회
-  get_list: `${serverURL}/reservations/get_list`, // 사용자 예약 내역 조회
+  get_list: (user_Id: string) => `${BASE_URL}/reservations/get_list?user_id=${user_Id}`, // 사용자 예약 내역 조회
 });
 
 /**
