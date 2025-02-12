0ce2357b25c42e3e240bc8b59a07baa9bd509af4
kim dong hyeon <kdh990315@naver.com>
2025-02-12 23:53:30 +0900

    fix: 파일 확장자 변경

--- src/utils/dayFormat.ts
+++ src/utils/dayFormat.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
 // "202509221030" 형식을 dayjs 객체로 변환
-export const reservationTime = (date) => {
+export const formatDate = (date: string) => {
   const formattedData = dayjs(
     `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)} ${date.slice(8, 10)}:${date.slice(
       10,
