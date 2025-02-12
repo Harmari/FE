5a746b16d5680b78f7b4b218553a83b256285acb
kim dong hyeon <kdh990315@naver.com>
2025-02-12 22:12:08 +0900

    feat: reservation type 정의

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -12,3 +12,13 @@ export interface Designer {
   non_face_consulting_fee: number;
   updated_at: string;
 }
+
+export interface Reservation {
+  id: string;
+  designer_id: string;
+  user_id: string;
+  consulting_fee: number;
+  mode: "대면" | "비대면";
+  reservation_date_time: string;
+  status: "예약완료" | "예약취소" | "상담완료";
+}
