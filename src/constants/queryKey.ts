55781c07871f8cc01f5c473d6a8bf9edcf04213e
kim dong hyeon <kdh990315@naver.com>
2025-02-12 22:23:30 +0900

    feat: 쿼리 키 추가

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -2,8 +2,13 @@ const DESIGNER = {
   list: ["designerList"],
 };
 
+const RESERVATION = {
+  list: (user_id: string) => ["reservationList", { user_id }],
+};
+
 const QUERY_KEY = {
   designer: DESIGNER,
+  reservationList: RESERVATION,
 };
 
 export default QUERY_KEY;
