de25f0512f3660daee0cf3e4167f6e236070bd9b
Sean <joajoa70584@gmail.com>
2025-02-12 20:37:15 +0900

    feat: 결제, 예약 페이지 라우터 연결

---
+++ src/routes/route/DESIGNER_ROUTES.tsx
@@ -0,0 +1,11 @@
+import { PATH } from "@/constants/path";
+import DesignerListPage from "@/pages/DesignerListPage";
+
+const DESIGNER_ROUTES = [
+  {
+    path: PATH.designerList,
+    element: <DesignerListPage />,
+  },
+];
+
+export default DESIGNER_ROUTES;
