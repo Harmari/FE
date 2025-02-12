134bffb402e2314a213ffc3d34c8937cd0b3aa65
kim dong hyeon <kdh990315@naver.com>
2025-02-12 15:25:52 +0900

    feat: json-server 디자이너 목록 구조 변경 및 타입 변경

--- src/pages/DesignerListPage/components/DesignerList.tsx
+++ src/pages/DesignerListPage/components/DesignerList.tsx
@@ -2,17 +2,17 @@ import { Designer } from "@/types/apiTypes";
 import DesignerItem from "./DesignerItem";
 
 interface DesignerListProps {
-	designers: Designer[];
+  designers: Designer[];
 }
 
 const DesignerList = ({ designers }: DesignerListProps) => {
-	return (
-		<div className="flex flex-col gap-[23px]">
-			{designers.map((designer) => (
-				<DesignerItem key={designer.id} designer={designer} />
-			))}
-		</div>
-	)
-}
+  return (
+    <div className="flex flex-col gap-[23px]">
+      {designers.map((designer) => (
+        <DesignerItem key={designer._id} designer={designer} />
+      ))}
+    </div>
+  );
+};
 
 export default DesignerList;
--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -1,11 +1,14 @@
 export interface Designer {
-	id: string;
-	name: string;
-	address: string;
-	area: string;
-	specialty: string;
-	faceToFacePrice: number;
-	nonFaceToFacePrice: number;
-	consultationType: string[];
-	introduction: string;
+  _id: string;
+  name: string;
+  region: string;
+  specialties: string;
+  introduction: string;
+  profile_image: string;
+  available_modes: string;
+  created_at: string;
+  face_consulting_fee: number;
+  shop_address: string;
+  non_face_consulting_fee: number;
+  updated_at: string;
 }
