cecd776cef06a880670c8772de0536ed8cb5b4bc
Sean <joajoa70584@gmail.com>
2025-02-13 00:15:43 +0900

    style: footer 글자 제거

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,5 @@
 const Footer = () => {
-  return (
-    <footer className="sticky bottom-0 w-full h-20 bg-gray-scale-200 mt-2">푸터 영역입니다.</footer>
-  );
+  return <footer className="sticky bottom-0 w-full h-16 bg-gray-scale-200 mt-2"></footer>;
 };
 
 export default Footer;
