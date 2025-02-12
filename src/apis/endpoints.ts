a1fa7201748a9350e0016b1dea8cb4c21d3cd52c
kim dong hyeon <kdh990315@naver.com>
2025-02-12 21:32:25 +0900

    feat: get_list api 추가

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -5,15 +5,29 @@ export const localURL = "http://localhost:3001";
 
 interface ReservationEndpoint {
   list: string;
+  get_list: string;
 }
 
 interface LocalEndpoint {
   designers: string;
 }
 
+/**
+# ======================#
+|       RESERVATION     |
+# ======================#
+*/
+
 export const RESERVATION_ENDPOINT: ReservationEndpoint = Object.freeze({
-  list: `${serverURL}/reservations/list`,
+  list: `${serverURL}/reservations/list`, // 디자이너 예약 목록 조회
+  get_list: `${serverURL}/reservations/get_list`, // 사용자 예약 내역 조회
 });
+
+/**
+# ======================#
+|         LOCAL         |
+# ======================#
+*/
 
 export const LOCAL_ENDPOINT: LocalEndpoint = Object.freeze({
   designers: `${localURL}/designers`,
