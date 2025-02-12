50a87d3cc35ec25ef7e0bec4db1d1b7e685bdb92
Sean <joajoa70584@gmail.com>
2025-02-13 01:39:39 +0900

    style: footer fixed로 스타일 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className="sticky bottom-0 w-full h-16 bg-gray-scale-200 mt-2"></footer>;
+  return <footer className="fixed bottom-0 w-[375px] h-16 bg-gray-scale-200 mt-2"></footer>;
 };
 
 export default Footer;
