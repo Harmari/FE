ba4393cb8d3e8c79435fdc532a36519c1afab71f
Sean <joajoa70584@gmail.com>
2025-02-13 00:04:09 +0900

    style: footer 스타일 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,7 @@
 const Footer = () => {
-	return (
-		<div className="h-[90px] bg-slate-500 mt-2">
-			푸터 영역입니다.
-		</div>
-	)
-}	
+  return (
+    <footer className="sticky bottom-0 w-full h-20 bg-gray-scale-200 mt-2">푸터 영역입니다.</footer>
+  );
+};
 
 export default Footer;
