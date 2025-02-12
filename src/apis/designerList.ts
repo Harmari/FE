5d3d08671d4987354544b721c0e96ecbe44b0252
kim dong hyeon <kdh990315@naver.com>
2025-02-12 14:05:27 +0900

    feat: 디자이너 리스트 ui

--- src/apis/designerList.ts
+++ src/apis/designerList.ts
@@ -6,7 +6,7 @@ export const getDesignerList = async () => {
 	try {
 		const response = await axios.get(LOCAL_ENDPOINT.designers);
 
-		console.log(response.data);
+		return response.data;
 	} catch (error) {
 		// 추후 에러 핸들링 필요
 		console.log(error)
