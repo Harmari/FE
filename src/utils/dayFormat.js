10c9c282619f770da62c6dd2ddb4a5f8c16cbee0
kim dong hyeon <kdh990315@naver.com>
2025-02-12 23:44:19 +0900

    feat: utils폴더 dayFormat 유틸함수 추가

---
+++ src/utils/dayFormat.js
@@ -0,0 +1,13 @@
+import dayjs from "dayjs";
+
+// "202509221030" 형식을 dayjs 객체로 변환
+export const reservationTime = (date) => {
+  const formattedData = dayjs(
+    `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)} ${date.slice(8, 10)}:${date.slice(
+      10,
+      12
+    )}`
+  );
+
+  return formattedData;
+};
