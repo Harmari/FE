fd577b5fa76f89af57cc7d9c593d94b024cdab79
kim dong hyeon <kdh990315@naver.com>
2025-02-13 16:32:55 +0900

    style: 디자이너 리스트 font-size 변경

--- src/pages/DesignerListPage/components/DesignerItem.tsx
+++ src/pages/DesignerListPage/components/DesignerItem.tsx
@@ -11,18 +11,18 @@ const DesignerItem = ({ designer }: DesignerItemProps) => {
     <div className="bg-white ">
       <div className="border-b border-gray-200 py-[22px] px-[28px] flex justify-between">
         <div className="w-[166px] ">
-          <p className="text-[10px] mb-1 text-[#737373]">{designer.shop_address}</p>
-          <p className="text-[16px] mb-2">{designer.name}</p>
-          <p className="text-[14px] text-[#676767]">{designer.introduction}</p>
+          <p className="text-[10px] mb-2 text-[#737373]">{designer.shop_address}</p>
+          <p className="text-[15px] mb-2">{designer.name}</p>
+          <p className="text-[13px] text-[#676767]">{designer.introduction}</p>
           <div className="flex gap-2 mt-4">
-            <div className="bg-[#D896FF] rounded-full px-2 py-1">
+            <div className="bg-[#D896FF] rounded-full px-2 py-[2px]">
               <span className="text-[12px] text-white whitespace-nowrap">
                 {designer.specialties} 전문
               </span>
             </div>
             {designerModeFormat(designer.available_modes as DesignerMode)?.map((mode) => (
-              <div className="bg-[#f4f4f4] rounded-full px-2 py-1 whitespace-nowrap" key={mode}>
-                <span className="text-[12px]">{mode}</span>
+              <div className="bg-[#f4f4f4] rounded-full px-2 py-[2px] whitespace-nowrap" key={mode}>
+                <span className="text-[11px]">{mode}</span>
               </div>
             ))}
           </div>
