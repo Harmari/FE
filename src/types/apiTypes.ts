c7bad63585d80a098bdecd9c052c3c157dbfab47
Sean <joajoa70584@gmail.com>
2025-02-14 00:29:51 +0900

    feat: user 인터페이스 추가

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -23,3 +23,16 @@ export interface Reservation {
   reservation_date_time: string;
   status: "예약완료" | "예약취소" | "상담완료";
 }
+
+export interface User {
+  email: string;
+  google_id: string;
+  name: string;
+  profile_image: string;
+  provider: string;
+  status: string;
+  refresh_token: string;
+  created_at: string;
+  updated_at: string;
+  id: string;
+}
