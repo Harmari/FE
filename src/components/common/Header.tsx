2416b38ff123745a6307e11cd9ae1a009862acf0
kim dong hyeon <kdh990315@naver.com>
2025-02-13 14:26:01 +0900

    feat: 헤더 컴포넌트 생성 및 root layout 수정

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full pt-7 px-4">
+      <div className="w-[117px] h-[29px] bg-[url('images/main-logo.png')] bg-contain bg-no-repeat bg-center"></div>
+    </header>
+  );
+};
+
+export default Header;
