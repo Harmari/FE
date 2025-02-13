e7f57e7750a3b0ea29dbdef247775775b24d1674
kim dong hyeon <kdh990315@naver.com>
2025-02-13 17:37:54 +0900

    feat: 디자이너 필터 퍼블리싱

--- src/pages/DesignerListPage/components/DesignerFilterDrawer.tsx
+++ src/pages/DesignerListPage/components/DesignerFilterDrawer.tsx
@@ -6,41 +6,58 @@ const DesignerFilterDrawer = () => {
 
   console.log(data);
 
-  return (
-    <div className="text-center px-8">
-      {/* 대면 컨설팅 방식 필터 */}
-      <div className="pb-4">
-        <h3 className="text-[32px] pb-2">컨설팅 방식</h3>
+  const locationList = ["서울 전체", "홍대/연남/합정", "강남/청담/압구정", "성수/건대"] as const;
 
-        <div className="flex justify-around">
-          <div className="px-6 py-4 bg-gray-500 rounded-md">
-            <p>대면</p>
-            <p>2만원</p>
+  return (
+    <div className="pt-4">
+      {/* 대면 컨설팅 방식 필터 */}
+      <div className="w-full border-b border-[#F2F2F2]">
+        <div className="mb-[18px]">
+          <strong className="block text-[16px] mb-[6px]">컨설팅 방식</strong>
+          <p className="text-[12px] text-[#868686] leading-[16px]">
+            소요시간 약 30분 <br />
+            컨설팅 내용은 진행 후 요약된 리포트를 고객에게 전달됩니다.
+          </p>
+        </div>
+
+        <div className="flex gap-2 pb-7">
+          <div className="border border-[#F2F2F2] py-4 px-4 w-[50%]">
+            <p>
+              대면 <span className="text-[#D9D9D9]">(30,000원~)</span>
+            </p>
+            <p className="text-[10px]">실제 샵에 방문하여 컨설팅 진행</p>
           </div>
-          <div className="px-6 py-4 bg-gray-500 rounded-md">
+          <div className="border border-[#F2F2F2] py-4 px-4 w-[50%] flex flex-col justify-center items-center">
             <p>비대면</p>
-            <p>3만원</p>
           </div>
         </div>
       </div>
 
       {/* 지역 필터 */}
-      <div className="pb-4">
-        <h3 className="text-[32px] pb-2">지역</h3>
+      <div className="w-full border-b border-[#F2F2F2] pb-4">
+        <div className="my-4">
+          <strong className="block text-[16px] mb-[6px]">지역</strong>
+        </div>
 
-        <div className="grid grid-cols-2 gap-4">
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">강남</div>
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">청담</div>
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">압구정</div>
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">홍대</div>
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">연남</div>
-          <div className="px-6 py-4 bg-gray-500 rounded-lg">합정</div>
+        <div className="flex flex-wrap gap-2 text-[14px]">
+          {locationList.map((location) => (
+            <div
+              key={location}
+              className={`rounded-full px-2 py-1 ${
+                location === "서울 전체" ? "bg-[#D896FF]" : "bg-[#F0F0F0]"
+              }`}
+            >
+              {location}
+            </div>
+          ))}
         </div>
       </div>
 
       {/* 가격 필터 */}
-      <div>
-        <h3 className="text-[32px] pb-2">가격</h3>
+      <div className="w-full border-b border-[#F2F2F2] pb-4">
+        <div className="my-4">
+          <strong className="block text-[16px] mb-[6px]">지역</strong>
+        </div>
 
         <div className="w-[80%] mx-auto">
           <div className="relative pt-6">
