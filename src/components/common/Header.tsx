3320a8d1c670fbf3090ad96d9e0ea82078a93fef
Sean <joajoa70584@gmail.com>
2025-02-14 00:30:29 +0900

    refactor: header 스타일 변경 후 홈 레이아웃에서 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="w-full pt-7 px-4">
-      <div className="w-[117px] h-[29px] bg-[url('images/main-logo.png')] bg-contain bg-no-repeat bg-center"></div>
+    <header className="w-full pt-7 px-4 mb-7">
+      <Link
+        to="/"
+        className="block w-[117px] h-[29px] bg-[url('images/main-logo.png')] bg-contain bg-no-repeat bg-center"
+      ></Link>
     </header>
   );
 };
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/common/Header";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
     <div className="w-[393px] min-h-dvh m-auto bg-gray-50">
       <div className="font-sans">
-        <Header />
         <Outlet />
       </div>
     </div>
