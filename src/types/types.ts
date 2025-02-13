ab2993685ede5b00cb2a81aacb16bcfe467ce4d7
kim dong hyeon <kdh990315@naver.com>
2025-02-13 14:45:50 +0900

    type: 디자이너 필터 타입 수정

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,8 @@
 // 타입 정의
+export type DesignerMode = "대면" | "비대면";
+
 export type FilterOptions = {
-  designer_location: string;
-  designer_pricer: string;
-  designer_mode: string;
+  designer_location?: string;
+  designer_pricer?: string;
+  designer_mode?: DesignerMode[];
 };
