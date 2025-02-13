c09bc13056e7d9003e2039432ccef2b2267b5d77
kim dong hyeon <kdh990315@naver.com>
2025-02-13 15:20:43 +0900

    feat: 디자이너 리스트 필터

--- src/pages/DesignerListPage/components/filteredOptionbox.tsx
+++ src/pages/DesignerListPage/components/filteredOptionbox.tsx
@@ -1,5 +1,10 @@
-const FilteredOptionBox = () => {
-  return <div>FilteredOptionBox</div>;
+const FilteredOptionBox = ({ option }: { option: string }) => {
+  return (
+    <div className="text-[#868686] text-[14px] flex items-center gap-1">
+      <span>{option}</span>
+      <img src="images/delete_option.svg" alt="옵션 삭제" />
+    </div>
+  );
 };
 
 export default FilteredOptionBox;
