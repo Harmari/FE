5870cdfe44088f075daed5b608b58e0a70b0f616
kim dong hyeon <kdh990315@naver.com>
2025-02-14 00:50:01 +0900

    style: 필터 디자인 변경

--- src/pages/DesignerListPage/components/DesignerFilterDrawer.tsx
+++ src/pages/DesignerListPage/components/DesignerFilterDrawer.tsx
@@ -1,10 +1,19 @@
 import { Slider } from "@/components/ui/slider";
 import { useState } from "react";
+import { DesignerFilterMode, DesignerLocation, FilterOptions } from "@/types/types";
 
-const DesignerFilterDrawer = () => {
+interface DesignerFilterDrawerProps {
+  filterOptions: FilterOptions;
+  handleModeChange: (mode: DesignerFilterMode) => void;
+  handleLocationChange: (location: DesignerLocation) => void;
+}
+
+const DesignerFilterDrawer = ({
+  filterOptions,
+  handleModeChange,
+  handleLocationChange,
+}: DesignerFilterDrawerProps) => {
   const [data, setData] = useState(20000);
-
-  console.log(data);
 
   const locationList = ["서울 전체", "홍대/연남/합정", "강남/청담/압구정", "성수/건대"] as const;
 
@@ -21,13 +30,26 @@ const DesignerFilterDrawer = () => {
         </div>
 
         <div className="flex gap-2 pb-7">
-          <div className="border border-[#F2F2F2] py-4 px-4 w-[50%]">
+          {/* 대면 */}
+          <div
+            className={`border border-[#F2F2F2] py-4 px-4 w-[50%] cursor-pointer ${
+              filterOptions.designer_mode === "대면" ? "bg-[#F9ECFF]" : ""
+            }`}
+            onClick={() => handleModeChange("대면")}
+          >
             <p>
               대면 <span className="text-[#D9D9D9]">(30,000원~)</span>
             </p>
             <p className="text-[10px]">실제 샵에 방문하여 컨설팅 진행</p>
           </div>
-          <div className="border border-[#F2F2F2] py-4 px-4 w-[50%] flex flex-col justify-center items-center">
+
+          {/* 비대면 */}
+          <div
+            className={`border border-[#F2F2F2] py-4 px-4 w-[50%] flex flex-col justify-center items-center cursor-pointer ${
+              filterOptions.designer_mode === "비대면" ? "bg-[#F9ECFF]" : ""
+            }`}
+            onClick={() => handleModeChange("비대면")}
+          >
             <p>비대면</p>
           </div>
         </div>
@@ -43,9 +65,12 @@ const DesignerFilterDrawer = () => {
           {locationList.map((location) => (
             <div
               key={location}
-              className={`rounded-full px-2 py-1 ${
-                location === "서울 전체" ? "bg-[#D896FF]" : "bg-[#F0F0F0]"
+              className={`rounded-full px-2 py-1 cursor-pointer ${
+                filterOptions.designer_location?.includes(location)
+                  ? "bg-[#D896FF]"
+                  : "bg-[#F0F0F0]"
               }`}
+              onClick={() => handleLocationChange(location)}
             >
               {location}
             </div>
