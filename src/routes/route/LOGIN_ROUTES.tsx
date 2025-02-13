cb17fc45242d95357a0ce36cf4789ee8f9730542
Sean <joajoa70584@gmail.com>
2025-02-13 15:08:45 +0900

    feat: 성공, 실패 페이지 라우터에 연결

--- src/routes/route/LOGIN_ROUTES.tsx
+++ src/routes/route/LOGIN_ROUTES.tsx
@@ -1,10 +1,23 @@
 import { PATH } from "@/constants/path";
-import { LoginPage } from "@/pages";
+import { LoginPage, AuthSuccessPage, AuthErrorPage } from "@/pages";
 
 const LOGIN_ROUTES = [
   {
     path: PATH.login,
     element: <LoginPage />,
+  },
+  // {
+  //   path: PATH.authCallback,
+  //   element: <CallbackPage />,
+  // },
+  {
+    path: PATH.authSuccess,
+    element: <AuthSuccessPage />,
+  },
+
+  {
+    path: PATH.authError,
+    element: <AuthErrorPage />,
   },
 ];
 
