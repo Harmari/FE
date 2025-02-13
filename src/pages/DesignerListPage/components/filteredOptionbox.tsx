035ed83fa69e43fb3ea59a8b3cda592dde9b15e5
kim dong hyeon <kdh990315@naver.com>
2025-02-14 00:50:27 +0900

    style: 필터 디자인 수정

--- src/pages/DesignerListPage/components/filteredOptionbox.tsx
+++ src/pages/DesignerListPage/components/filteredOptionbox.tsx
@@ -2,7 +2,9 @@ const FilteredOptionBox = ({ option }: { option: string }) => {
   return (
     <div className="text-[#868686] text-[13px] flex items-center gap-1">
       <span>{option}</span>
-      <img src="images/delete_option.svg" alt="옵션 삭제" className="w-4 h-4" />
+      {option !== "서울 전체" && (
+        <img src="images/delete_option.svg" alt="옵션 삭제" className="w-4 h-4" />
+      )}
     </div>
   );
 };
