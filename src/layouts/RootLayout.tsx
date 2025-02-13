66df84151d844b6382c56583636488a809c57bbc
kim dong hyeon <kdh990315@naver.com>
2025-02-13 14:26:01 +0900

    feat: 헤더 컴포넌트 생성 및 root layout 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/common/Header";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
     <div className="w-[375px] min-h-dvh m-auto bg-gray-50">
       <div className="font-sans">
+        <Header />
         <Outlet />
       </div>
     </div>
