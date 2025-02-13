eb2f3dd695f619e2851255c74fc9bc22338ae7cf
Sean <joajoa70584@gmail.com>
2025-02-14 00:32:11 +0900

    refactor: 예약 상세페이지 디자이너 정보(주소, 구글 미트 링크) 추가

--- src/pages/ReservationDetailPage/index.tsx
+++ src/pages/ReservationDetailPage/index.tsx
@@ -1,29 +1,26 @@
 import { getReservationDetail } from "@/apis/reservationDetail";
-import Footer from "@/components/common/Footer";
 import QUERY_KEY from "@/constants/queryKey";
 import { useQuery } from "@tanstack/react-query";
 import ReservationInfo from "./components/ReservationInfo";
 import ImageSection from "./components/ImageSection";
+import { useParams } from "react-router-dom";
+import { Reservation } from "@/types/apiTypes";
 
-const reservation_id = "67ab4a82e38533dcf975950e";
-
-const ReservationDetailPage = () => {
-  const { data, isPending, isError } = useQuery({
-    queryKey: QUERY_KEY.reservationList.detail(reservation_id),
-    queryFn: () => getReservationDetail(reservation_id),
-    staleTime: 1000 * 60 * 5,
-    gcTime: 1000 * 60 * 60,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-  });
-
-  let content;
+const ReservationDetailContent = ({
+  data,
+  isPending,
+  isError,
+}: {
+  data: Reservation | undefined;
+  isPending: boolean;
+  isError: boolean;
+}) => {
   if (isPending) {
-    content = <div className="px-5">Loading...</div>;
+    return <div className="px-5">Loading...</div>;
   }
 
   if (isError) {
-    content = (
+    return (
       <div>
         <h2>데이터를 불러오지 못했습니다.</h2>
         <p>예약 상세 정보를 불러오지 못했습니다. 잠시 후에 다시 시도해주세요.</p>
@@ -32,8 +29,7 @@ const ReservationDetailPage = () => {
   }
 
   if (data) {
-    console.log(data);
-    content = (
+    return (
       <>
         <ImageSection />
         <ReservationInfo reservation={data} />
@@ -41,11 +37,25 @@ const ReservationDetailPage = () => {
     );
   }
 
+  return null;
+};
+
+const ReservationDetailPage = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: QUERY_KEY.reservationList.detail(id as string),
+    queryFn: () => getReservationDetail(id as string),
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
+
   return (
     <div className="pb-24">
       <h2 className="pt-4 px-5 text-xl font-bold mb-4">예약 상세정보</h2>
-      {content}
-      <Footer />
+      <ReservationDetailContent data={data} isPending={isPending} isError={isError} />
     </div>
   );
 };
