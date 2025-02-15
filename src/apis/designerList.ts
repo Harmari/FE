176edc7ad0d9fd7463e8f41d13f7bd7ab6b72973
kim dong hyeon <kdh990315@naver.com>
2025-02-15 23:02:56 +0900

    feat: 디자이너 리스트 api 호출 변경

--- src/apis/designerList.ts
+++ src/apis/designerList.ts
@@ -1,9 +1,19 @@
+import { FilterOptions } from "@/types/types";
 import { DESIGNER_ENDPOINT } from "./endpoints";
 import devApi from "@/config/axiosDevConfig";
+import qs from "qs";
 
-export const getDesignerList = async () => {
+export const getDesignerList = async (filterOptions: FilterOptions) => {
   try {
-    const response = await devApi.get(DESIGNER_ENDPOINT.designers);
+    const response = await devApi.get(DESIGNER_ENDPOINT.designers, {
+      params: {
+        region: filterOptions.designer_location,
+        available_modes: filterOptions.designer_mode,
+      },
+      paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+    });
+
+    // console.log(params);
 
     return response.data;
   } catch (error) {
