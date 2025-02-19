9df76df989a6d3527e923911275e90d1f272eec0
Sean <joajoa70584@gmail.com>
2025-02-19 16:45:09 +0900

    Hotfix: 회원탈퇴 api 엔드포인트 수정

--- src/apis/userDelete.ts
+++ src/apis/userDelete.ts
@@ -2,7 +2,7 @@ import devApi from "@/config/axiosDevConfig";
 
 export const userDelete = async () => {
   try {
-    const response = await devApi.delete("/user/delete");
+    const response = await devApi.delete("/user/me");
     return response.data;
   } catch (error) {
     console.error(error);
