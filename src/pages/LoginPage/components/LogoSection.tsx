be2ff7240ca6c1cdb27f6d36413d604b491e1d2a
Sean <joajoa70584@gmail.com>
2025-02-20 10:01:27 +0900

    style: 로고 사이즈 변경

--- src/pages/LoginPage/components/LogoSection.tsx
+++ src/pages/LoginPage/components/LogoSection.tsx
@@ -1,10 +1,10 @@
 const LogoSection = () => {
   return (
     <section className="flex flex-col items-center justify-center">
-      <h1 className="overflow-hidden m-auto mb-1">
+      <h1 className="w-3/4 overflow-hidden m-auto mb-1">
         <img src="/images/white-logo-svg.svg" alt="logo image" className="size-full object-cover" />
       </h1>
-      <p className=" text-white text-[13px] tracking-[1px]">실패하지 않는 헤어컨설팅</p>
+      <p className=" text-white text-sub-title">실패하지 않는 헤어컨설팅</p>
     </section>
   );
 };
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="min-h-dvh px-12 flex flex-col justify-center gap-[217px] bg-[#D36DF5]">
+      <div className="min-h-dvh px-12 flex flex-col justify-center gap-40 bg-[#D36DF5]">
         <LogoSection />
         <ButtonSection />
       </div>
